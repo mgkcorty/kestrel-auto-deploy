60fdab7f44b6c0f328200e43b24ad241b055436c
Danil Shaykhutdinov <mgkcorty@gmail.com>
2022-04-30 20:42:16 +0500

    Refactoring.

--- src/kestrel-auto-deploy.py
+++ src/kestrel-auto-deploy.py
@@ -10,6 +10,7 @@ from time import sleep
 
 WINDOWS = 'Windows'
 LINUX = 'Linux'
+MACOSX = 'Darwin'
 
 CURRENT_PLATFORM = platform.system()
 CONFIG_FILE_NAME = 'kestrel-auto-deploy-config-win.json' if CURRENT_PLATFORM == WINDOWS else 'kestrel-auto-deploy-config.json'
@@ -31,13 +32,11 @@ class ProcessInfo:
     pid = ""
     name = ""
     version = ""
-    command_line = ""
 
-    def __init__(self, pid, name, version, command_line):
+    def __init__(self, pid, name, version):
         self.pid = pid
         self.name = name
         self.version = version
-        self.command_line = command_line
 
 
 def copy3(src, dst, *, follow_symlinks=True):
@@ -50,25 +49,13 @@ def copy3(src, dst, *, follow_symlinks=True):
     return dst
 
 
-def is_dirty_app_version():
+def is_old_app_version():
     if not os.path.exists(REMOTE_VERSION_NUMBER_FILE):
         raise Exception(f'{VERSION_NUMBER_FILE_NAME} not found in remote folder {REMOTE_VERSION_NUMBER_FILE}.')
     remote_last_modify_time = os.path.getmtime(REMOTE_VERSION_NUMBER_FILE)
     local_last_modify_time = os.path.getmtime(LOCAL_VERSION_NUMBER_FILE) if os.path.exists(
         LOCAL_VERSION_NUMBER_FILE) else 0
     return remote_last_modify_time > local_last_modify_time
-
-
-def initialize():
-    if CURRENT_PLATFORM != WINDOWS and CURRENT_PLATFORM != LINUX:
-        raise Exception('Platform is not supported.')
-
-    if CURRENT_PLATFORM == LINUX:
-        user = os.getenv("SUDO_USER")
-        if user is None:
-            user = os.getenv("USER")
-        if user is None:
-            raise Exception('Current user is None.')
 
 
 def represents_int(s):
@@ -83,66 +70,47 @@ def remove_spaces(lines):
     return list(map(lambda x: " ".join(x.strip().split()), lines))
 
 
-def get_process_infos(name):
+def get_process_info(process_text):
     if CURRENT_PLATFORM == WINDOWS:
-        lines = subprocess.getoutput('wmic process where caption="dotnet.exe" get Commandline, ProcessId').split('\n')
-        lines = list(map(lambda x: " ".join(x.strip().split()), lines))
-
-        def get_process_info(s):
-            i = len(s) - 1
-            start_index = -1
-            while i > 0:
-                if s[i] == " ":
-                    start_index = i + 1
-                    break
-                i -= 1
-            pid = s[start_index:]
-            if represents_int(pid):
-                command_arguments = s[:start_index - 1]
-                handle_utility_path = os.path.dirname(os.path.realpath(__file__))
-                version_result = subprocess.run([f"handle", "-p", "dotnet.exe"], cwd=handle_utility_path,
-                                                universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                version_result = list(filter(lambda x: x, version_result.stdout.split('\n')))
-                version_result = remove_spaces([version_result[5]])[0]
-                length = len(version_result)
-                version = version_result[length - 7:]
-                return ProcessInfo(pid, name, version, command_arguments)
-            return None
-
-        process_infos = list(filter(lambda pair: pair is not None, map(lambda x: get_process_info(x), lines)))
-        return process_infos
-    elif CURRENT_PLATFORM == LINUX:
-        process_infos = []
-        try:
-            lines = check_output(["ps -eo pid,cmd | grep [d]otnet"], shell=True, universal_newlines=True).split('\n')
-            lines = remove_spaces(lines)
-        except subprocess.CalledProcessError as e:
-            if (e.returncode != 1):
-                raise
-            return process_infos
-
-        def get_process_info(s):
-            i = 0
-            end_index = -1
-            while i < len(s):
-                if s[i] == " ":
-                    end_index = i
-                    break
-                i += 1
-            pid = s[:end_index]
-            if represents_int(pid):
-                command_arguments = s[end_index + 1:]
-                length = len(command_arguments)
-                version_result = check_output([f"pwdx {pid}"], shell=True, universal_newlines=True).split('\n')
-                version_result = remove_spaces(version_result)[0]
-                version = version_result[length - 8:]
-                return ProcessInfo(pid, name, version, command_arguments)
-            return None
-
-        process_infos = list(filter(lambda pair: pair is not None, map(lambda x: get_process_info(x), lines)))
-        return process_infos
+        i = len(process_text) - 1
+        start_index = -1
+        while i > 0:
+            if process_text[i] == " ":
+                start_index = i + 1
+                break
+            i -= 1
+        pid = process_text[start_index:]
+        if represents_int(pid):
+            handle_utility_path = os.path.dirname(os.path.realpath(__file__))
+            version_result = subprocess.run([f"handle", "-p", "dotnet.exe"], cwd=handle_utility_path,
+                                            universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            version_result = list(filter(lambda x: x, version_result.stdout.split('\n')))
+            version_result = remove_spaces([version_result[5]])[0]
+            length = len(version_result)
+            version = version_result[length - 7:]
+            return ProcessInfo(pid, version, version)
+        return None
     else:
-        raise Exception('getPid not implemented for current platform.')
+        components = process_text.split(" ")
+        pid = components[0]
+        executable_name = components[-1]
+        if represents_int(pid):
+            version_result = check_output([f"pwdx {pid}"], shell=True, universal_newlines=True).split('\n')
+            version_result = remove_spaces(version_result)[0]
+            version = version_result.split("/")[-1]
+            return ProcessInfo(pid, executable_name, version)
+        return None
+
+
+def get_process_list(process_name):
+    if CURRENT_PLATFORM == WINDOWS:
+        terminal_process_list = subprocess.getoutput('wmic process where caption="dotnet.exe" get Commandline, ProcessId').split('\n')
+    else:
+        terminal_process_list = check_output(["ps -eo pid,cmd | grep [d]otnet"], shell=True, universal_newlines=True).split('\n')
+    terminal_process_list = remove_spaces(terminal_process_list)
+    process_info_list = list(map(lambda x: get_process_info(x), terminal_process_list))
+    result = list(filter(lambda p: p is not None and p.name == process_name, process_info_list))
+    return result
 
 
 def process_runner():
@@ -151,8 +119,9 @@ def process_runner():
     file = open(LOCAL_VERSION_NUMBER_FILE, 'r')
     current_version = file.read()
     file.close()
-    infos = get_process_infos(EXECUTABLE_FILE_NAME)
+    infos = get_process_list(EXECUTABLE_FILE_NAME)
     same_version_processes = list(filter(lambda x: x.version == current_version, infos))
+    print(len(same_version_processes))
     if len(same_version_processes) > 0:
         same_version_process = same_version_processes[0]
     else:
@@ -169,12 +138,10 @@ def process_runner():
         process = subprocess.run([DOTNET_PATH, EXECUTABLE_FILE_NAME], cwd=site_directory, stdout=subprocess.PIPE,
                                  stderr=subprocess.PIPE,
                                  creationflags=subprocess.DETACHED_PROCESS | subprocess.CREATE_NEW_PROCESS_GROUP)
-    elif CURRENT_PLATFORM == LINUX:
+    else:
         print(f"Process starting: dotnet {site_directory}")
         process = subprocess.run([DOTNET_PATH, EXECUTABLE_FILE_NAME], cwd=site_directory, stdout=subprocess.PIPE,
                                  stderr=subprocess.PIPE, preexec_fn=os.setpgrp)
-    else:
-        raise Exception('subprocess is not implemented for current platform.')
     print(process.stdout)
 
 
@@ -201,7 +168,7 @@ def copytree(src, dst):
 
 
 def version_update():
-    if not is_dirty_app_version():
+    if not is_old_app_version():
         return
 
     file = open(REMOTE_VERSION_NUMBER_FILE, 'r')
@@ -214,7 +181,7 @@ def version_update():
     copytree(os.path.join(REMOTE_FOLDER, current_version), os.path.join(LOCAL_FOLDER, current_version))
     copy3(os.path.join(REMOTE_FOLDER, VERSION_NUMBER_FILE_NAME),
           os.path.join(LOCAL_FOLDER, VERSION_NUMBER_FILE_NAME))
-    infos = get_process_infos(EXECUTABLE_FILE_NAME)
+    infos = get_process_list(EXECUTABLE_FILE_NAME)
     for info in infos:
         os.kill(int(info.pid), signal.SIGTERM)
     directories = [d for d in os.listdir(LOCAL_FOLDER) if os.path.isdir(os.path.join(LOCAL_FOLDER, d))]
@@ -242,7 +209,15 @@ def version_update_loop():
 
 
 def main():
-    initialize()
+    if CURRENT_PLATFORM != WINDOWS and CURRENT_PLATFORM != LINUX and CURRENT_PLATFORM != MACOSX:
+        raise Exception('Platform is not supported.')
+
+    if CURRENT_PLATFORM == LINUX or CURRENT_PLATFORM == MACOSX:
+        user = os.getenv("SUDO_USER")
+        if user is None:
+            user = os.getenv("USER")
+        if user is None:
+            raise Exception('Current user is None.')
 
     threads = [Thread(target=version_update_loop, args=()), Thread(target=process_runner_loop, args=())]
     for t in threads:
